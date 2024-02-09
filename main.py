86756f50effc9634e13545b25cfc1b9696cd1f21
SirRamNick <ramenicholas.tiongson@olopsc.edu.ph>
2024-02-10 05:01:46 +0800

    updated 2

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-
 letter = '''
-    Hi babi love mahal baby ko! Feb 6, 2024, marks six years and nine months since we embarked on this incredible journey together. Thank you for filling the past 81 months with so much love and laughter. I'm grateful for every one of the 352 weeks and 3 days we've shared, and for the 2,467 days of pure joy and happiness you've brought into my life
+Hi babi love mahal irog kalandian ko! Feb 6, 2024, marks six years and nine months since we embarked on this incredible journey together. Thank you for filling the past 81 months with so much love and laughter. I'm grateful for every one of the 352 weeks and 3 days we've shared, and for the 2,467 days of pure joy and happiness you've brought into my life.
 
 Being with you feels like snuggling up in a cozy blanket, wrapped in happiness, peace, and love. Every moment with you is like a sprinkle of magic, and I'll hold onto these memories forever.
 
@@ -11,26 +10,35 @@ Life can get crazy, but you're my anchor, my source of strength. Your love and s
 As we reflect on our journey together, I find myself longing for more adventures with you by my side. Will you be my Valentine, my partner in crime? Let's create even more beautiful memories together and continue this incredible journey of love.
 '''
 
-
-def get_result(user_name, password):
+def get_result(user_name, password, response):
     if user_name == 'ramlovejade' and password == 'iloveyoulove06':
-        st.empty()
         return letter
-
 
 def main():
     st.title("💙")
-    
+
     # Text input
     with st.form(key='my_form'):
-        user_name = st.text_input("Enter something", "")
-        password = st.text_input("Enter something", "", type="password")
+        user_name = st.text_input("Enter username", "")
+        password = st.text_input("Enter password", "", type="password")
         submitted = st.form_submit_button("Submit")
+
         if submitted:
-            result1 = get_result(user_name, password)
+            response = st.text_input("(yes/no)", "")
+
+            if response == 'yes':
+                st.markdown("## yeheyyyy 👉👌")
+            elif response == 'no':
+                st.markdown('## you mean yes? -_-')
+            elif response == 'luh':
+                st.markdown('## luh i love you')
+
+            result = get_result(user_name, password, response)
             st.markdown("## From adi 🌻")
-            st.write(result1)
+            st.write(result)
             st.write("From your adi")
+
+
 
 if __name__ == "__main__":
     main()
